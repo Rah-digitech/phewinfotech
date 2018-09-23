96e70aff9f04f0daeb757e2fd884681a851975df
Rah-digitech <43511021+Rah-digitech@users.noreply.github.com>
2018-09-23 12:23:42 +0530

    first upload

---
+++ Quick-Enquiry.php
@@ -0,0 +1,74 @@
+<?php
+
+session_start();
+
+if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$_SESSION["code"]==$_POST["captcha"])
+{
+$name			=$_REQUEST['txtNames'];
+$mobile			=$_REQUEST['txtMobiles'];
+$email1			=$_REQUEST['txtEmails'];
+$message		=$_REQUEST['txtQueries'];
+
+
+
+$message1="<font><b>Quick Enquiry </b></font><br>";
+
+$message1.="<br><b>Name :</b><font color='blue'>&nbsp;".$name."</font>";
+
+$message1.="<br><b>Mobile. :</b><font color='blue'>&nbsp;".$mobile."</font>";
+$message1.="<br><b>Email Id. :</b><font color='blue'>&nbsp;".$email1."</font>";
+$message1.="<br><b>Message :</b><font color='blue'>&nbsp;".$message."</font>";
+
+$email="info@red-pixels.com";
+
+
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+$headers .= 'From: noreply@red-pixels.com'. " \r\n";
+
+$res= mail($email,'Quick Enquiry ',$message1,$headers);
+
+
+$msg='<img src="img/thanks.jpg" class="img-responsive img-thumbnail" alt=""><h4 style="color:#000;">';
+
+
+} else {
+
+$msg='<h2 style="color:#FF0000">Please Enter Correct Captcha Code. </h2>';
+}
+?>
+<!DOCTYPE html>
+<html xml:lang="en-US" lang="en-US" dir="ltr">
+
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Best Graphic | Web Designing | Multimedia Training Course in Delhi (Laxmi Nagar)</title>
+    <meta name="description" content="Delhi's Best Multimedia Training Institute for Graphic, Web Designing, Desktop Publishing-DTP, C, C++, Java, Sketching, Painting, Photoshop, Illustrator, Indesign, CorelDraw, AutoCAD, Audio/Video Editing.">
+    <meta name="keywords" content="red pixels, red pixels contact, contact us, call us, red pixels direction">
+	<meta name="abstract" content="Graphic, Web design course with 100% Job placement. Join industry best graphic design course taday." />
+    <meta name="author" content="www.red-pixels.com">
+	
+	<link rel="canonical" href="https://www.red-pixels.com/red-pixels-contact"/>
+<link href="css/custom.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+</head>
+<body>
+
+<div class="container"><br><br>
+<center><?=$msg?></center>	
+</div>   
+ 
+<script>
+setTimeout(function() {
+  window.location.href = "http://www.red-pixels.com/";
+}, 3000);
+</script>
+ 
+</body>
+</html>
---
+++ bussiness.php
@@ -0,0 +1,51 @@
+<?php include("db.php");?>               
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title> About Us | </title> 
+<meta name="keywords" content="">
+<meta name="description" content="">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- For IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <?php include 'header.php';?>
+
+<section class="rev_slider_wrapper">
+    
+</section>
+<!--End rev slider wrapper--> 
+    
+<!--Start welcome area-->
+<section class="welcome-area">
+    <div class="container clearfix">
+        <div class="sec-title">
+            <h2>Business Consultancy</h2>
+            <span class="decor"></span>
+        </div>
+        <div class="row">
+            <div class="col-md-5">
+                     <img src="img/bussiness.jpg" alt="Awesome Image" class="img-responsive">
+					 </div>
+					 <div class="col-md-7">
+					 <div class="text-holder">
+                    <p align="justify">Opportunity knocks only once. We here at Mentation technology strongly believe that identifying a new business is more critical than running it.</p>
+
+<p align="justify">Mentation Technology is also operational as an established business consultant which offers you new business opportunities prevalent in market and which holds all possible future potentials. You can expect budget Franchise opportunity where an effective team will guide you right from identification to complete business set up and solution, backed up with marketing support and guidance.  We aim at providing you both not only new business opportunity but also can guide you to take over a running business or partner as an Master Franchise. We feel proud to have eminent partners like Meatwale Foods, Mr Foodie express, Stanfort school, Little Millenium, Poddar Brio School , 1 Weblab , Fresh n Frozen etc for immediate and established Franchise opportunity. 
+</p>
+                    
+                </div>
+            </div>
+            
+            
+        </div>
+    </div>
+</section>
+
+
+
+
+            <?php include 'footer.php';?>
+			
